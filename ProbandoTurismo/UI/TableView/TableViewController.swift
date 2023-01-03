b9ca38fdb791e467e3cde6991ed19c4331de51a8
Markel <markjuaristi@gmail.com>
2023-01-03 13:10:51 +0100

    tablebar con la lista de dias

---
+++ ProbandoTurismo/UI/TableView/TableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TebleViewController.swift
+//  ProbandoTurismo
+//
+//  Created by Markel Juaristi on 28/12/22.
+//
+
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let singleRows = ["lunes", "martes", "miercoles", "jueves","viernes", "sabado", "domingo"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /* con esto hacemos que la clase TableviewController herede de UITAbleview delegate y datasource*/
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    //Delegate and DataSource methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return singleRows.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        /* indexPath es la posivcion de la celda*/
+        let weekDayName = singleRows[indexPath.row] /* el parametro row nos mostrara la misma cantidad de elementos de tenemos en la lista singlerows*/
+       
+        cell.textLabel?.text = weekDayName
+        return cell
+    }
+    
+
+     
+
+}
